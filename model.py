ce057d19437a50f8c06802968cc5c24102a6ad95
Michael Paglia <michaelpaglia02@gmail.com>
2023-12-26 17:11:12 -0500

    add file

---
+++ model.py
@@ -0,0 +1,5 @@
+"""
+Show that the mean of the centered data matrix Din Eq.(1.9) is 0
+
+The mean of the centered data matrix D is 0 ∈ Rd, because we have subtracted the mean µ from all the points xi.
+"""
